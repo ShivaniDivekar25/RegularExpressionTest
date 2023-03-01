dbb2f262cbc19457a410558d63a8c2b6f45d0d2e
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-01 09:10:51 +0530

    Modified to Validate First Name Test program

---
+++ RegularExpressionTest/RegexPattern.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressionTest
+{
+    public class RegexPattern
+    {
+        public string ValidateFirstName(string name)
+        {
+            string firstName = "^[A-z]{1}[a-z]{2,}$";
+            return TestPattern(firstName, name);
+        }
+        public string TestPattern(string firstName,string name)
+        {
+            if (Regex.IsMatch(name, firstName))
+            {
+                return "Valid";
+            }
+            else
+            {
+                return "Invalid";
+            }
+        }
+    }
+}
---
+++ RegularExpressionTestProgram/RegularExpression.cs
@@ -0,0 +1,16 @@
+using RegularExpressionTest;
+
+namespace RegularExpressionTestProgram
+{
+    public class Tests
+    {
+        RegexPattern regex = new RegexPattern();
+        [Test]
+        public void ShouldReturn_FirstName_ValidOrNot()
+        {
+            string result = regex.ValidateFirstName("Shivani");
+            Assert.AreEqual("Valid", result);
+            Assert.Pass();
+        }
+    }
+}
